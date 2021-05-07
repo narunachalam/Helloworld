5b9213e1e2c4215d858229704471af1f22df6c0b
narunachalamatma <niraimathy.arunachalam@mass.gov>
2021-05-06 21:44:53 -0400

    pr

--- index.js
+++ index.js
@@ -10,6 +10,44 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  const { Octokit } = require('@octokit/rest')
+
+  const octokit = new Octokit({
+    baseUrl: 'https://api.github.com',
+    auth: "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",
+    log: {
+      debug: () => { },
+      info: () => { },
+      warn: console.warn,
+      error: console.error
+    },
+    request: {
+      agent: undefined,
+      fetch: undefined,
+      timeout: 0
+    }
+  })
+
+  const owner = "executive-office-of-education"
+  const repo = "EOEAAAService"
+  const prNumber = 189
+
+  const run = async () => {
+    try {
+
+      const pullRequests = await octokit.pulls.listCommits({ owner: owner, repo: repo, pull_number: prNumber })
+
+      pullRequests.data.forEach((pr) => {
+        const location = pr.commit.url.search("commits/")
+        console.log('message', pr.commit.url.substr(location + "commits/".length, 7), ' ', pr.commit.message)
+      })
+    } catch (e) {
+      console.log("Cannot find PR", `${owner}/${repo}#${prNumber}`, e.status, e.message)
+      return null
+    }
+  }
+
+  run()
 } catch (error) {
   core.setFailed(error.message);
 }
